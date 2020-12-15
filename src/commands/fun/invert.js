11c94ce0642aa246376da683326af646e945cb3a
Tegnio6882 <olegbubble@gmail.com>
2020-12-15 23:37:26 +0300

    New command: invert

---
+++ src/commands/fun/invert.js
@@ -0,0 +1,25 @@
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "invert",
+  description: "",
+  category: "fun",
+  cooldown: 2,
+  async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const member = bot.findMember(message, args, true);
+    const image = member.user.displayAvatarURL({ size: 512, format: "png", dynamic: true });
+    const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
+    const data = `https://some-random-api.ml/canvas/invert?&avatar=${image}`;
+
+    const embed = BaseEmbed(message)
+    .setTitle(lang.IMAGE.FAILED_TO_LOAD)
+    .setURL(data)
+    .setImage(data);
+
+    setTimeout(() => {
+      message.channel.send(embed);
+      wait_msg.delete();
+    }, 100);
+  },
+};
