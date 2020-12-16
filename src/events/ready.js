5713ece22345feabf836987dc8c1faa822507cd3
Tegnio6882 <olegbubble@gmail.com>
2020-12-16 14:32:12 +0300

    Removed implementation of sdc-api & updated deps

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,9 +3,6 @@ const Logger = require("../modules/Logger");
 module.exports = {
   name: "ready",
   execute(bot) {
-    let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4MTE3OTgxMDcwMDk4NDMzMCIsImlhdCI6MTYwODExNzk1OH0.f2x7yuT0uEHZWRiTa31kX2zcfOx4OjC2LwvoDIy0gYw";
-    const SDC = require("sdc-api");
-    const sdc = new SDC(apiKey);
     const commands = bot.commands.size;
     const statuses = [
       `s)help | ${commands} ⚙️`,
@@ -16,7 +13,6 @@ module.exports = {
     setInterval(() => {
       const status = statuses[Math.floor(Math.random() * statuses.length)];
       bot.user.setActivity(status, { type: "WATCHING" });
-      sdc.setAutoPost(bot);
     }, 10000);
   },
 };
