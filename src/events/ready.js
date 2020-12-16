775aa27000b468bb0a8f1afc42d15f0157bbe343
Tegnio6882 <olegbubble@gmail.com>
2020-12-16 14:28:11 +0300

    Implementation of sdc-api

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,9 @@ const Logger = require("../modules/Logger");
 module.exports = {
   name: "ready",
   execute(bot) {
+    let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4MTE3OTgxMDcwMDk4NDMzMCIsImlhdCI6MTYwODExNzk1OH0.f2x7yuT0uEHZWRiTa31kX2zcfOx4OjC2LwvoDIy0gYw";
+    const SDC = require("sdc-api");
+    const sdc = new SDC(apiKey);
     const commands = bot.commands.size;
     const statuses = [
       `s)help | ${commands} ⚙️`,
@@ -13,6 +16,7 @@ module.exports = {
     setInterval(() => {
       const status = statuses[Math.floor(Math.random() * statuses.length)];
       bot.user.setActivity(status, { type: "WATCHING" });
+      sdc.setAutoPost(bot);
     }, 10000);
   },
 };
