7827c7efcc4b1b2424318174a143fd7b639304bc
Tegnio6882 <olegbubble@gmail.com>
2020-12-17 15:04:56 +0300

    Added bufferutil & animated status

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,8 +4,12 @@ module.exports = {
   name: "ready",
   execute(bot) {
     const commands = bot.commands.size;
+    const servers = bot.guilds.cache.size;
+    const users = bot.users.cache.size;
     const statuses = [
       `s)help | ${commands} ⚙️`,
+      `s)help | ${servers} 🗃️`,
+      `s)help | ${users} 👥`,
     ];
 
     Logger.log("bot", "Bot is running!");
@@ -13,6 +17,6 @@ module.exports = {
     setInterval(() => {
       const status = statuses[Math.floor(Math.random() * statuses.length)];
       bot.user.setActivity(status, { type: "WATCHING" });
-    }, 10000);
+    }, 1000);
   },
 };
