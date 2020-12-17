65315d716f13356af1b3cb0868d4e25b431e07cd
Tegnio6882 <olegbubble@gmail.com>
2020-12-17 19:30:43 +0300

    Auto-update stats in animated status

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,18 +3,17 @@ const Logger = require("../modules/Logger");
 module.exports = {
   name: "ready",
   execute(bot) {
-    const commands = bot.commands.size;
-    const servers = bot.guilds.cache.size;
-    const users = bot.users.cache.size;
-    const statuses = [
-      `s)help | ${commands} ⚙️`,
-      `s)help | ${servers} 🗃️`,
-      `s)help | ${users} 👥`,
-    ];
-
     Logger.log("bot", "Bot is running!");
     bot.user.setStatus("idle");
     setInterval(() => {
+      const commands = bot.commands.size;
+      const servers = bot.guilds.cache.size;
+      const users = bot.users.cache.size;
+      const statuses = [
+        `s)help | ${commands} ⚙️`,
+        `s)help | ${servers} 🗃️`,
+        `s)help | ${users} 👥`,
+      ];
       const status = statuses[Math.floor(Math.random() * statuses.length)];
       bot.user.setActivity(status, { type: "WATCHING" });
     }, 10000);
