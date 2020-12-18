c2e035d032c415266d45d3a1bd5d077023830a4e
Tegnio6882 <olegbubble@gmail.com>
2020-12-18 12:06:18 +0300

    New command: snow

---
+++ src/commands/fun/snow.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "snow",
+  description: "",
+  category: "fun",
+  cooldown: 2,
+  async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const member = bot.findMember(message, args, true);
+    const image = member.user.displayAvatarURL({ size: 512, format: "png" });
+    const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
+    const data = `https://api.no-api-key.com/api/v2/snow?image=${image}`;
+
+    const embed = BaseEmbed(message)
+    .setTitle(lang.IMAGE.FAILED_TO_LOAD)
+    .setURL(data)
+    .setImage(data);
+
+    setTimeout(() => {
+      message.channel.send(embed);
+      wait_msg.delete();
+    }, 100);
+  },
+};
