04623d4feb7eeda9f365096809ca978b1c9ec63d
Tegnio6882 <olegbubble@gmail.com>
2020-12-19 23:09:26 +0300

    New command: github

---
+++ src/commands/useful/github.js
@@ -0,0 +1,50 @@
+const fetch = require("node-fetch");
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "github",
+  description: "",
+  category: "useful",
+  cooldown: 5,
+  aliases: ["gh"],
+  async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const username = args[0];
+
+    if (!username) {
+      return message.channel.send(lang.GLOBAL.PROVIDE_ARGS);
+    }
+
+    const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
+    const url = `https://api.github.com/users/${username}`;
+    const result = await fetch(url).then((res) => res.json());
+    const user = result;
+
+    if (user?.message === "Not Found") {
+      return message.channel.send(lang.OTHER.GH_NOT_FOUND)
+        .then(() => msg.delete());
+    }
+
+    msg.delete();
+
+    const twitter = user.twitter_username
+      ? `[@${user.twitter_username}](https://twitter.com/${user.twitter_username})`
+      : lang.GLOBAL.NOT_SPECIFIED;
+    const website = user.blog ? user.blog : lang.GLOBAL.NOT_SPECIFIED;
+    const location = user.location ? user.location : lang.GLOBAL.NOT_SPECIFIED;
+    const bio = user.bio ? user.bio : lang.GLOBAL.NOT_SPECIFIED;
+
+    const embed = BaseEmbed(message)
+      .setTitle(user.login)
+      .addField("Twitter", twitter, true)
+      .addField(lang.OTHER.GH_FOLLOWING, user.following, true)
+      .addField(lang.OTHER.GH_FOLLOWERS, user.followers, true)
+      .addField(lang.OTHER.GH_WEBSITE, website, true)
+      .addField(lang.OTHER.GH_LOCATION, location, true)
+      .addField(lang.OTHER.URL, user.html_url)
+      .setDescription(`${lang.OTHER.GH_BIO}: ${bio}`)
+      .setThumbnail(user.avatar_url);
+
+    message.channel.send(embed);
+  },
+};
