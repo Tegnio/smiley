144215c829e5af0db93db813a9414a53a1069b87
Tegnio6882 <olegbubble@gmail.com>
2020-12-19 17:34:10 +0300

    Added 'randomcolor' command

---
+++ src/commands/useful/randomcolor.js
@@ -0,0 +1,20 @@
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "randomcolor",
+  description: "",
+  category: "util",
+  cooldown: 2,
+  aliases: ["rcolor", "rclr"],
+  execute(bot, message) {
+    const color = Math.floor(Math.random()*16777215).toString(16);
+    const url = `https://api.no-api-key.com/api/v2/color?hex=${color}`;
+
+    const embed = BaseEmbed(message)
+      .setColor(`#${color}`)
+      .setThumbnail(url)
+      .setTitle(`#${color}`);
+
+    message.channel.send(embed);
+  },
+};
