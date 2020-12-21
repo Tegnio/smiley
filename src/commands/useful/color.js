9ad72dc2da096c58c8e397b1a25b3a0bdd6f90f6
Tegnio6882 <olegbubble@gmail.com>
2020-12-21 16:20:30 +0300

    Args length check in 'color' command

--- src/commands/useful/color.js
+++ src/commands/useful/color.js
@@ -1,4 +1,5 @@
 const BaseEmbed = require("../../modules/BaseEmbed");
+const { getGuildById } = require("../../utils/functions");
 
 module.exports = {
   name: "color",
@@ -7,6 +8,7 @@ module.exports = {
   cooldown: 2,
   aliases: ["clr"],
   async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
     const rcolor = Math.floor(Math.random()*16777215).toString(16);
     const url = `https://api.no-api-key.com/api/v2/color?hex=`;
     const color = args[0];
@@ -18,11 +20,13 @@ module.exports = {
       .setTitle(`#${rcolor}`);
 
       message.channel.send(embed);
+    } else if(color.length > 6) {
+        return message.channel.send(lang.GLOBAL.LONG_ARGS.replace("{limit}", "6"));
     } else {
-      const embed = BaseEmbed(message)
-      .setColor(color)
-      .setThumbnail(url + color)
-      .setTitle(`#${color}`);
+        const embed = BaseEmbed(message)
+        .setColor(color)
+        .setThumbnail(url + color)
+        .setTitle(`#${color}`);
 
       message.channel.send(embed);
     };
