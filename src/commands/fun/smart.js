eb8f45775640ac0456f86cef8a0b2a2fdfed0d26
Tegnio6882 <olegbubble@gmail.com>
2020-12-21 12:22:13 +0300

    Testing new fun command...

---
+++ src/commands/fun/smart.js
@@ -0,0 +1,28 @@
+const fetch = require("node-fetch");
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "smart",
+  description: "",
+  category: "fun",
+  cooldown: 2,
+  async execute(bot, message, args) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const member = bot.findMember(message, args, true);
+    const image = member.user.displayAvatarURL({ size: 512, format: "png" });
+    const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
+    const data = await fetch(`https://api.no-api-key.com/api/v2/smrt?image=${image}`).then((res) =>
+      res.json()
+    ).then(console.log);
+
+    const embed = BaseEmbed(message)
+    .setTitle(lang.IMAGE.FAILED_TO_LOAD)
+    .setURL(data)
+    .setImage(data.message);
+
+    setTimeout(() => {
+      message.channel.send(embed);
+      wait_msg.delete();
+    }, 100);
+  },
+};
