129fff001115ed25bac5c2e111e95b39e91731c4
Tegnio6882 <olegbubble@gmail.com>
2020-12-23 21:37:19 +0300

    Added args check to 'clyde' command

--- src/commands/fun/clyde.js
+++ src/commands/fun/clyde.js
@@ -9,7 +9,12 @@ module.exports = {
   async execute(bot, message, args) {
     const lang = await bot.getGuildLang(message.guild.id);
     const text = args.join(" ");
-    if (!text) return message.channel.send(lang.GLOBAL.PROVIDE_ARGS);
+    if(!text) return message.channel.send(lang.GLOBAL.PROVIDE_ARGS);
+    if(text.length > 80) {
+      return message.channel.send(lang.GLOBAL.LONG_ARGS
+      .replace("{length}", text.length)
+      .replace("{limit}", "80"));
+    }
 
     const data = await fetch(
       `https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`
@@ -19,7 +24,7 @@ module.exports = {
       .setTitle(lang.IMAGE.FAILED_TO_LOAD)
       .setURL(data.message)
       .setImage(data.message);
-      
+
     message.channel.send(embed);
   },
 };
