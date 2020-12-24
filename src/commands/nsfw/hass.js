e0edff983ec18de311e255610005b0b4ebc71fb7
Tegnio6882 <olegbubble@gmail.com>
2020-12-24 14:38:16 +0300

    New command: hass

---
+++ src/commands/nsfw/hass.js
@@ -0,0 +1,27 @@
+const fetch = require("node-fetch");
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "hass",
+  description: "",
+  category: "nsfw",
+  cooldown: 5,
+  nsfwOnly: true,
+  async execute(bot, message) {
+    const lang = await bot.getGuildLang(message.guild.id);
+    const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
+    const data = await fetch(
+      "https://nekobot.xyz/api/image?type=hass"
+    ).then((res) => res.json());
+
+    const embed = BaseEmbed(message)
+    .setTitle(lang.IMAGE.FAILED_TO_LOAD)
+    .setURL(data.message)
+    .setImage(data.message);
+
+    setTimeout(() => {
+      message.channel.send(embed);
+      wait_msg.delete();
+    }, 100);
+  },
+};
