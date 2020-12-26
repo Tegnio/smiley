2f4ea709cfdc4d74ab0f17aa3831dfe0f57d4ed3
Tegnio6882 <olegbubble@gmail.com>
2020-12-26 14:26:24 +0300

    Updated ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -16,18 +16,7 @@ module.exports = {
         `s)help | ${users} 👥`,
       ];
       const status = statuses[Math.floor(Math.random() * statuses.length)];
-      bot.user.setActivity(status, { type: "WATCHING" });
-
+      bot.user.setActivity(status, { type: "LISTENING" });
     }, 15000);
-
-    setInterval(() => {
-      fetch(`https://senko-info.ga/api/roflan/haudi`)
-        .then((res) => res.json())
-        .then((data) => {
-          const text = data.text;
-
-        bot.channels.cache.get('721803638510583839').send(text);
-      });
-    }, 3600000);
   },
 };
