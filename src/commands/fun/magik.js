c6567129a0bd0da9823367457228fdb078ab7dea
Tegnio6882 <olegbubble@gmail.com>
2020-12-26 14:06:58 +0300

    Some fixes

--- src/commands/fun/magik.js
+++ src/commands/fun/magik.js
@@ -12,7 +12,7 @@ module.exports = {
     const member = bot.findMember(message, args, true);
     const image = member.user.displayAvatarURL({ size: 512, format: "png" });
     const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
-    const intensity = Math.floor(Math.random() * 9 + 1)
+    const intensity = Math.floor(Math.random() * 10);
     const data = await fetch(`https://nekobot.xyz/api/imagegen?type=magik&image=${image}&intensity=${intensity}`)
       .then((res) => res.json());
 
