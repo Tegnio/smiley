fd668c2a00ea1d41a55d9dd91b085c662587cb05
Tegnio6882 <olegbubble@gmail.com>
2020-12-28 18:59:15 +0300

    Quick fix

--- src/commands/fun/wave.js
+++ src/commands/fun/wave.js
@@ -11,9 +11,9 @@ module.exports = {
     const member = bot.findMember(message, args, true);
     const image = member.user.displayAvatarURL({ size: 512, format: "png" });
     const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
-    let amount = args[1] || Math.floor(Math.random() * 9 + 1));
+    let amount = args[1] || Math.floor(Math.random() * 9 + 1);
     if (member.user.id === message.author.id) {
-      amount = args[0] || Math.floor(Math.random() * 9 + 1));
+      amount = args[0] || Math.floor(Math.random() * 9 + 1);
     }
     const data = `https://useless-api--vierofernando.repl.co/wave?image=${image}&amount=${amount}`;
 
