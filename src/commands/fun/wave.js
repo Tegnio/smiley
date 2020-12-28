93bc6b977a96cba00f1eb374d64faae07c0ae20a
Tegnio6882 <olegbubble@gmail.com>
2020-12-28 18:56:41 +0300

    Updated wave.js

--- src/commands/fun/wave.js
+++ src/commands/fun/wave.js
@@ -11,7 +11,11 @@ module.exports = {
     const member = bot.findMember(message, args, true);
     const image = member.user.displayAvatarURL({ size: 512, format: "png" });
     const wait_msg = await message.channel.send(lang.OTHER.PROCESSING);
-    const data = `https://useless-api--vierofernando.repl.co/wave?image=${image}&amount=${Math.floor(Math.random() * 9 + 1)}`;
+    let amount = args[1] || Math.floor(Math.random() * 9 + 1));
+    if (member.user.id === message.author.id) {
+      amount = args[0] || Math.floor(Math.random() * 9 + 1));
+    }
+    const data = `https://useless-api--vierofernando.repl.co/wave?image=${image}&amount=${amount}`;
 
     const embed = BaseEmbed(message)
     .setTitle(lang.IMAGE.FAILED_TO_LOAD)
