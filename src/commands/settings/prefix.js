1ef356789b0ea6a7eb646fb21eee53cc75ead59f
Tegnio6882 <olegbubble@gmail.com>
2020-12-29 13:41:19 +0300

    Big Undo

--- src/commands/settings/prefix.js
+++ src/commands/settings/prefix.js
@@ -6,7 +6,7 @@ module.exports = {
   name: "prefix",
   description: "",
   category: "settings",
-  cooldown: 5,
+  cooldown: 2,
   memberPermissions: ["MANAGE_GUILD"],
   async execute(bot, message, args) {
     const prefix = args[0];
