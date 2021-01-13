23cc27471c5179f5dd15fc71b2b8eebc871b5da5
Tegnio6882 <olegbubble@gmail.com>
2021-01-13 11:33:57 +0300

    New command: notstonks

---
+++ src/commands/fun/notstonks.js
@@ -0,0 +1,26 @@
+const BaseEmbed = require("../../modules/BaseEmbed");
+
+module.exports = {
+  name: "notstonks",
+  description: "Allows you to get a Not stonks image with somebody's (or yours) avatar",
+  category: "fun",
+  aliases: ["nst"],
+  cooldown: 2,
+  args: "[user]",
+  botPermissions: ["ATTACH_FILES", "EMBED_LINKS"],
+  async execute(bot, message, args) {
+    message.channel.startTyping();
+    const lang = await bot.getGuildLang(message.guild.id);
+    const member = bot.findMember(message, args, true);
+    const image = member.user.displayAvatarURL({ size: 512, format: "png" });
+    const url = `https://vacefron.nl/api/stonks?user=${image}&notstonks=true`;
+
+    const embed = BaseEmbed(message)
+    .setTitle(lang.IMAGE.FAILED_TO_LOAD)
+    .setURL(url)
+    .setImage(url);
+
+    message.channel.stopTyping(true);
+    message.channel.send(embed);
+  },
+};
